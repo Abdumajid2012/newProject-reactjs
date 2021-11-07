4d09c20d9d94f41a5273e897462b1c5773421ad3
Abdumajid2012 <abdumajid6252@gmail.com>
2021-11-07 13:10:37 +0500

    bironBro

--- src/home/Alice.js
+++ src/home/Alice.js
@@ -1,12 +1,12 @@
 import React from "react";
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import image1 from "./freestocks-Y9mWkERHYCU-unsplash.jpg";
+import image1 from "./maxresdefault.jpg";
 import image2 from "./jackson-so-_t-l5FFH8VA-unsplash.jpg";
 import image from "./markus-spiske-MI9-PY5cyNs-unsplash.jpg";
-import image3 from "./nathan-dumlao-EdULZpOKsUE-unsplash.jpg";
+import image3 from "./css06.jpg";
 import image4 from "./nick-karvounis-TkZYCXmrKK4-unsplash.jpg";
-import image5 from "./oziel-gomez-L8-0SAy-aoQ-unsplash.jpg";
+import image5 from "./html.jpg";
 import {
     Card, Button, CardImg, CardTitle, CardText, CardDeck,
     CardSubtitle, CardBody, Container, Col, Row
@@ -41,11 +41,12 @@ const Alice = () => {
             <Col md="4">
             <CardDeck>
       <Card className="flex">
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
         <CardBody>
           <CardTitle tag="h5">Card title</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <CardText>HTML - Hyper Text Markup Language (Gipermatnli belgilanishlar tili) deb nomlanadi.
+            HTML dasturlash tili bo`lmagani uchun ham uning kodlariga buyruq emas, belgilanish sifatida qaraladi.
+          </CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
@@ -54,11 +55,12 @@ const Alice = () => {
             <Col md="4">
             <CardDeck>
       <Card className="flex">
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
         <CardBody>
           <CardTitle tag="h5">Card title</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <CardText>CSS (Cascading Steyle Sheets - sahifaning pog`onali shamoyili) - bu obyektlarga bezak va jilo berishga mo`ljallangan .
+            CSS dan foydalaning tezkorlikni oshirishda ham  katta ro`l o`ynaydi.
+          </CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
@@ -67,11 +69,12 @@ const Alice = () => {
             <Col md="4">
             <CardDeck>
       <Card className="flex">
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
         <CardBody>
           <CardTitle tag="h5">Card title</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <CardText>JavaScript - dunyodagi eng ommalop va juda keng tatbiq etiladigan web dasturlash tilidir.
+            JavaScript tilida tuzilgan dasturlar HTML hujjatining ichiga joylashtirilib ular bilan birga uzatiladi. 
+          </CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
